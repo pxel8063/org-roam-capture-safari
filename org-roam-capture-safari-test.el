d69d8ac82c053a8ca745fcc823c36a09420d4211
github <83626280+pxel8063@users.noreply.github.com>
2024-03-09 11:39:00 +0900

    make flycheck happy

--- org-roam-capture-safari-test.el
+++ org-roam-capture-safari-test.el
@@ -1,4 +1,6 @@
 ;;; org-roam-capture-safari-test.el --- test functions -*- lexical-binding: t -*-
+
+;;; Code:
 (require 'org-roam-capture-safari)
 
 (ert-deftest remove-leading-brackets-test ()
@@ -8,3 +10,6 @@
 (ert-deftest extract-title-test ()
   (let ((url "[[foo][bar]]"))
     (should (string= (org-roam-capture-safari-extract-title url) "bar"))))
+
+(provide 'org-roam-capture-safari-test)
+;;; org-roam-capture-safari-test.el ends here.

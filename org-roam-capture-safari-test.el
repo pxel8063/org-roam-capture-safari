540554e9f9d4fce948b93d7cd00e746517d4c95f
github <83626280+pxel8063@users.noreply.github.com>
2024-03-10 22:18:31 +0900

    edit documentation

--- org-roam-capture-safari-test.el
+++ org-roam-capture-safari-test.el
@@ -1,5 +1,24 @@
 ;;; org-roam-capture-safari-test.el --- test functions -*- lexical-binding: t -*-
 
+;; Copyright (C) 2024  pxel8063
+
+;; Author: pxel8063 <pxel8063@gmail.com>
+
+;; URL: https://github.com/pxel8063/org-roam-capture-safari
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Code:
 (require 'org-roam-capture-safari)
 
